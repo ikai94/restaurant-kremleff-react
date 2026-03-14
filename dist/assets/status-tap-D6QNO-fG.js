7cd5582520a8c34b999909cae952d219e2d87cb6
ikai94 <kotylevsliyy@yandex.ru>
2026-03-14 22:32:39 +0300

    update dist

--- dist/assets/status-tap-D6QNO-fG.js
+++ dist/assets/status-tap-D6QNO-fG.js
@@ -1,3 +1,3 @@
-import{r as a,a as i,w as c}from"./index-CvLHm-LL.js";import{f as d,s as l}from"./index8-Dj_M7KbX.js";/*!
+import{r as a,a as i,w as c}from"./index-C2pcLIpX.js";import{f as d,s as l}from"./index8-DC_QmiVh.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const p=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=d(o);t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{p as startStatusTap};
